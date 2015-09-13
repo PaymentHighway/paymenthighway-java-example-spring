17cff911334ec85ac02b6925a9f2c6a60010cd94
Terho Siikanen <terho.siikanen@solinor.com>
2015-09-13 20:04:59 +0300

    Use the Spring default path for properties. Add server.port property.

--- complete/src/main/java/io/paymenthighway/example/PaymentHighwaySettings.java
+++ complete/src/main/java/io/paymenthighway/example/PaymentHighwaySettings.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:config/paymenthighway.properties")
+@PropertySource("classpath:paymenthighway.properties")
 public class PaymentHighwaySettings {
 
     @Value("${paymenthighway.account}")
