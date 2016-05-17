21bfce413f4e33f3aa7688df3ea87347cbaf08d1
Terho Siikanen <terho.siikanen@solinor.com>
2016-05-17 15:33:25 +0300

    Improve deployment support

--- complete/src/main/java/io/paymenthighway/example/Application.java
+++ complete/src/main/java/io/paymenthighway/example/Application.java
@@ -1,10 +1,20 @@
 package io.paymenthighway.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
